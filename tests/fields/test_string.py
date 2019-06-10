a127b7444e24a1cd9f5e815832ae9f1d33c59eef
moreal <dev.moreal@gmail.com>
2019-06-10 09:44:21 +0900

    Add testcode for StringField

---
+++ tests/fields/test_string.py
@@ -0,0 +1,25 @@
+from pystructs import fields
+
+
+def test_string_utf8_encoding():
+    class CustomStruct(fields.Struct):
+        string = fields.StringField(11, 'utf8')
+
+    struct = CustomStruct(b'hello world')
+    assert struct.string == 'hello world'
+
+
+def test_string_ascii_encoding():
+    class CustomStruct(fields.Struct):
+        string = fields.StringField(11, 'ascii')
+
+    struct = CustomStruct(b'hello world')
+    assert struct.string == 'hello world'
+
+
+def test_string_euckr_encoding():
+    class CustomStruct(fields.Struct):
+        string = fields.StringField(10, 'euc-kr')
+
+    struct = CustomStruct(b'\xbe\xc8\xb3\xe7\xc7\xcf\xbc\xbc\xbf\xe4')
+    assert struct.string == '안녕하세요'
