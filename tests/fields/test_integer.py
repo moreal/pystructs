8cf9512d900acfafdea83e4dda87e7a5c6475bbd
moreal <dev.moreal@gmail.com>
2019-06-12 10:18:41 +0900

    Deprecate Struct and Add missing byteorder

--- tests/fields/test_integer.py
+++ tests/fields/test_integer.py
@@ -1,19 +1,17 @@
 from pystructs import fields
-from pystructs.fields.integer import Int32Field
-from pystructs.fields.struct import Struct
 
 
 def test_int32_field_little_byteorder():
-    class CustomStruct(Struct):
-        field = Int32Field(byteorder='little')
+    class CustomStruct(fields.ConstantStruct):
+        field = fields.Int32Field(byteorder='little')
 
     struct = CustomStruct(b'\x01\x00\x00\x00')
     assert struct.field == 1
 
 
 def test_int32_field_big_byteorder():
-    class CustomStruct(Struct):
-        field = Int32Field(byteorder='big')
+    class CustomStruct(fields.ConstantStruct):
+        field = fields.Int32Field(byteorder='big')
 
     struct = CustomStruct(b'\x00\x00\x00\x01')
     assert struct.field == 1
--- tests/fields/test_variable.py
+++ tests/fields/test_variable.py
@@ -4,7 +4,7 @@ from pystructs.fields.variable import VariableBytesField
 
 def test_variable_bytes_field_has_variable_size():
     class CustomStruct(VariableStruct):
-        length = Int32Field()
+        length = Int32Field(byteorder='big')
         data = VariableBytesField(related_field='length')
 
     struct = CustomStruct(b'\x00\x00\x00\x02\x12\x34')
