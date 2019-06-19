9aac717df6a536e737fe4adb2beef0e01a8a3158
moreal <dev.moreal@gmail.com>
2019-06-19 10:25:39 +0900

    Refactor Field

--- pystructs/fields/field.py
+++ pystructs/fields/field.py
@@ -1,13 +1,33 @@
+from __future__ import annotations
+
+
 class Field:
-    parent = None
-    offset = 0
+    __prev: Field
+    __bytes: bytes  # bytes of root field
+    __size: int
 
     def __init__(self, size: int):
-        self.size = size
+        self.__size = size
 
     def fetch(self):
         raise NotImplementedError()
 
     @property
     def is_root(self) -> bool:
-        return self.parent is None
+        return self.__prev is None
+
+    @property
+    def prev(self) -> Field:
+        return self.__prev
+
+    @prev.setter
+    def prev(self, field):
+        self.__prev = field
+
+    @property
+    def offset(self) -> int:
+        return self.__prev.offset + self.__prev.size
+
+    @property
+    def size(self) -> int:
+        return self.__size
