3f6fc599412fc2e5079ee01c7c2c4ab95126d262
moreal <dev.moreal@gmail.com>
2019-06-19 10:23:29 +0900

    Refactor BytesField

--- pystructs/fields/bytes.py
+++ pystructs/fields/bytes.py
@@ -2,5 +2,9 @@ from pystructs.fields.field import Field
 
 
 class BytesField(Field):
+    @property
+    def bytes(self):
+        return self.__bytes[self.offset:self.offset+self.size]
+
     def fetch(self) -> bytes:
-        return self.parent.bytes[self.offset:self.offset+self.size]
+        return self.bytes
