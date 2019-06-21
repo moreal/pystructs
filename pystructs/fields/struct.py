3c5507cf41350b032bb6c21731084bae4905276c
moreal <dev.moreal@gmail.com>
2019-06-21 11:48:44 +0900

    Refactor Struct

--- pystructs/fields/struct.py
+++ pystructs/fields/struct.py
@@ -1,39 +1,16 @@
+from __future__ import annotations
 from copy import deepcopy
 from typing import Dict, AnyStr, Union
 
 from pystructs import utils
-from pystructs.fields import IntField, BytesField
-from pystructs.fields.field import Field
-from pystructs.fields.variable import VariableBytesField
-from pystructs.interfaces import IConstant, IVariable
-from pystructs.utils import deepattr
+from pystructs.fields import BytesField, Field
 
 __all__ = [
-    'ConstantStruct',
-    'VariableStruct',
+    'Struct'
 ]
 
 
-class ConstantStructMetaclass(type):
-    def __new__(mcs, name, bases, attrs: dict):
-        attrs = utils.filter_fields(attrs)
-        attrs = utils.delete_fields(attrs)
-
-        offset = 0
-
-        for name, field in attrs['fields'].items():
-            if isinstance(field, IVariable):
-                raise TypeError("ConstantStruct can't have `IVariable` Field")
-
-            field.offset = offset
-            offset += field.size
-
-        attrs['size'] = offset
-
-        return super().__new__(mcs, name, bases, attrs)
-
-
-class VariableStructMetaclass(type):
+class StructMetaclass(type):
     def __new__(mcs, name, bases, attrs: dict):
         attrs = utils.filter_fields(attrs)
         attrs = utils.delete_fields(attrs)
@@ -41,19 +18,27 @@ class VariableStructMetaclass(type):
         return super().__new__(mcs, name, bases, attrs)
 
 
-class Struct(BytesField):
-    fields: Dict[Union[AnyStr, int], Field]
+class VirtualStruct:
+    def __init__(self, struct: Struct):
+        self.__struct = struct
 
     @property
-    def bytes(self):
-        return self._bytes
+    def offset(self) -> int:
+        return self.__struct.offset
+
+    @property
+    def size(self) -> int:
+        return 0
+
+
+class Struct(BytesField, metaclass=StructMetaclass):
+    fields: Dict[Union[AnyStr, int], Field]
 
     def __init__(self, _bytes: bytes = b''):
-        self._bytes = _bytes
-        self._initialize()
+        super().__init__(0)
+        self.__bytes = _bytes
 
-    def fetch(self):
-        self._bytes = self.parent.bytes[self.offset:self.offset+self.size]
+    def fetch(self) -> Struct:
         return self
 
     def __getattr__(self, item):
@@ -62,32 +47,21 @@ class Struct(BytesField):
         except KeyError:
             raise AttributeError(item)
 
-    def _initialize(self):
+    def initialize(self, root: Struct):
         self.fields = deepcopy(self.fields)
-        self._initialize_parent_of_fields()
+        for field in self.fields:
+            field.initialize(root)
+        self.__link_fields()
 
-    def _initialize_parent_of_fields(self):
-        for _, field in self.fields.items():
-            field.parent = self
+    def __link_fields(self):
+        fields = self.fields
+        fields_count = len(fields)
 
+        for index in range(fields_count - 1):
+            fields[index+1].prev = fields[index]
 
-class ConstantStruct(Struct, IConstant, metaclass=ConstantStructMetaclass):
-    pass
+        fields[0].prev = VirtualStruct(self)
 
-
-class VariableStruct(Struct, IVariable, metaclass=VariableStructMetaclass):
-    def _initialize(self):
-        self._initialize_parent_of_fields()
-        self._calculate_offset_and_size()
-
-    def _calculate_offset_and_size(self):
-        offset = 0
-
-        for name, field in self.fields.items():
-            if isinstance(field, VariableStruct) and not field.is_root:
-                field._initialize()
-
-            field.offset = offset
-            offset += field.size
-
-        self.size = offset
+    @property
+    def size(self) -> int:
+        return sum(map(lambda x: x.size, self.fields.values()))
