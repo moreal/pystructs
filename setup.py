6e59fd4dbc1126f4dcbc760a77e390c1f2d34481
moreal <dev.moreal@gmail.com>
2019-06-26 16:42:34 +0900

    chore: update 0.2.1 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 setup(
     url='https://github.com/moreal/pystructs',
     name='pystructs',
-    version='0.2.0',
+    version='0.2.1',
     long_description=long_description,
     long_description_content_type="text/markdown",
     description='c-like struct implementation for human',
